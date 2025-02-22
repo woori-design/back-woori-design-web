8796940db2b210590ecdefb9c60514b0afda0cfb
namsh1125 <namsh1125@naver.com>
2025-02-22 14:25:00 +0900

    feat: JPA auditing configuration 추가

---
+++ src/main/java/woori_design_web/back_woori_design_web/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package woori_design_web.back_woori_design_web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+}
