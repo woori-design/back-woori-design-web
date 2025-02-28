a38981502b1a48ad981aab60e634ee6f44fd5779
1223v <1223v@naver.com>
2025-02-28 17:30:48 +0900

    Feat: Entity 설계

--- src/main/java/woori_design_web/back_woori_design_web/entity/Like.java
+++ src/main/java/woori_design_web/back_woori_design_web/entity/Like.java
@@ -1,9 +1,14 @@
 package woori_design_web.back_woori_design_web.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "LIKE")
 public class Like {
 
     @Id
@@ -11,7 +16,13 @@ public class Like {
     private Long id;
 
     // 멤버
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     // 컴포넌트
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "components_id")
+    private Components components;
 
 }
--- src/main/java/woori_design_web/back_woori_design_web/entity/RefreshToken.java
+++ src/main/java/woori_design_web/back_woori_design_web/entity/RefreshToken.java
@@ -1,28 +1,77 @@
 package woori_design_web.back_woori_design_web.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-public class RefreshToken {
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "REFRESH_TOKEN")
+public class RefreshToken extends BaseTimeEntity {
 
     /**
-     * 리프레쉬 토큰 id
+     * 리프레시 토큰 id
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Member
+    /**
+     * 토큰 소유자
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
     /**
-     * 리프래쉬 토큰
+     * 리프레시 토큰 값
      */
-    private String refreshToken;
+    @Column(name = "token_value", nullable = false, unique = true)
+    private String value;
 
     /**
-     * 리프레쉬 토큰 만료 시간
+     * 리프레시 토큰 만료 시간
      */
-    private Date refreshTokenExpiresAt;
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
 
+    /**
+     * False : 사용 불가
+     * True : 사용 가능
+     */
+    @Column
+    private boolean isUsed;
+
+    /**
+     * 토큰의 유효성을 검사하는 메서드.
+     * <p>
+     * 1차적으로 토큰의 만료 시간을 기준으로 유효 여부를 판단하며,
+     * 추가적으로 이전에 만료 처리 요청이 있었는지 확인하는 데 사용된다.
+     *
+     * @return 토큰이 유효한 경우 true, 그렇지 않은 경우 false
+     */
+    public boolean isValid() {
+        return LocalDateTime.now().isBefore(expiresAt);
+    }
+
+    /**
+     * 토큰 만료 처리하는 메서드
+     */
+    public void expire() {
+        this.expiresAt = LocalDateTime.now();
+        this.isUsed = false;
+
+    }
+
+    protected void updateMember(Member member) {
+        this.member = member;
+    }
 }
