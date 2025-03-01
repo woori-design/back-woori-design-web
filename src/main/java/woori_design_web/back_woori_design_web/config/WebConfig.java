26e5cacfd2a22a198972aaabe3ceba2a8426339e
mjgwon24 <alswlchlrh8@naver.com>
2025-03-01 13:46:22 +0900

    update: KakaoConfig 추가 및 WebConfig RestTemplate 의존성 추가

--- src/main/java/woori_design_web/back_woori_design_web/config/WebConfig.java
+++ src/main/java/woori_design_web/back_woori_design_web/config/WebConfig.java
@@ -2,11 +2,17 @@ package woori_design_web.back_woori_design_web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
